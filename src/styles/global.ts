8a133060b5da8a71a32be0fee8a237727e7c96fc
matheus1714 <matheussilvamartins1714@gmail.com>
2024-01-24 22:38:04 -0300

    config default design

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,6 +5,9 @@ export const GlobalStyle = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+
+        font-family: 'Roboto', sans-serif;
+        font-size: 1rem;
     }
     body{
         background: ${(props) => props.theme.black};
--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,5 +1,36 @@
 export const defaultTheme = {
-    white: "#FFF",
+
+    whiteLight: "#fdfdfd",
+    whiteLightHover: "#fbfbfb",
+    whiteLightActivate: "#f7f7f7",
+    
+    white: "#e6e6e6",
+    whiteHover: "#cfcfcf",
+    whiteActivate: "#b8b8b8",
+
+    whiteDark: "#adadad",
+    whiteDarkHover: "#8a8a8a",
+    whiteDarkActivate: "#676767",
+
+    whiteDarker: "#515151",
+
+    // -------------------------
+
+    blueLight: "#e7eff7",
+    blueLightHover: "#dae7f3",
+    blueLightActivate: "#b3cee7",
+
     blue: "#0B60B0",
+    blueHover: "#0a569e",
+    blueActivate: "#094d8d",
+
+    blueDark: "#084884",
+    blueDarkHover: "#073a6a",
+    blueDarkActivate: "#052b4f",
+
+    blueDarker: "#04223e",
+
     black: "#000",
+
+
 }
