3ff937a5fecc8b7622a9abcb9d882b52b6658992
matheus1714 <matheussilvamartins1714@gmail.com>
2024-01-24 22:58:56 -0300

    update homepage with cursor animation

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,15 @@
-import { Container } from "./styled"
+import { Container, CursorAnimation, Phrase } from "./styled"
 
 export function Home(){
     return (
         <Container>
-            <h1>Home</h1>
+            <CursorAnimation>
+                <h1>Matheus <span>Mota</span></h1>
+            </CursorAnimation>
+            <Phrase>
+                <h2>“Constant improvement is better than delayed perfection”</h2>
+                <h2>Mark Tawain</h2>
+            </Phrase>
         </Container>
     )
 }
--- src/pages/Home/styled.ts
+++ src/pages/Home/styled.ts
@@ -1,3 +1,49 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-export const Container = styled.div``
+const cursor = keyframes`
+    50% {
+        border-color: transparent;
+    }
+`
+
+const typing = keyframes`
+    0% {width: 0}
+`
+
+export const Container = styled.div`
+`
+
+export const CursorAnimation = styled.div`
+    display: inline-block;
+
+    padding: 4rem 0;
+
+    h1{
+        font-size: 6rem;
+
+        letter-spacing: 10px;
+        border-right: 8px solid ${(props) => props.theme.blue};
+        width: 100%;
+        white-space: nowrap;
+        overflow: hidden;
+        animation:
+            ${typing} 2s,
+            ${cursor} .4s step-end infinite alternate;
+
+        span{
+            font-size: 6rem;
+            color: ${(props) => props.theme.blue};
+        }
+    }
+`
+
+export const Phrase = styled.div`
+    h2{
+        font-size: 2rem;
+        font-style: italic;
+
+        &:nth-child(2){
+            color: ${(props) => props.theme.blue};
+        }
+    }
+`
