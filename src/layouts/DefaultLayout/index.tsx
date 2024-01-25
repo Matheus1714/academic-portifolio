4d6b437465097a2573732c41f16c71aebf1fb2de
matheus1714 <matheussilvamartins1714@gmail.com>
2024-01-24 22:37:57 -0300

    add header and footer

--- src/layouts/DefaultLayout/index.tsx
+++ src/layouts/DefaultLayout/index.tsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom"
 import { Container } from "./styled"
+import { Header } from "../../components/Header"
+import { Footer } from "../../components/Footer"
 
 export function Layout(){
     return (
         <Container>
+            <Header />
             <Outlet />
+            <Footer />
         </Container>
     )
 }
--- src/layouts/DefaultLayout/styled.ts
+++ src/layouts/DefaultLayout/styled.ts
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-    
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+
+    padding: 0 5rem;
 `
