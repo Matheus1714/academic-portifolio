6a2053734c4e6f99729942e54b6dd97f20e26a7d
matheus1714 <matheussilvamartins1714@gmail.com>
2024-01-25 21:15:41 -0300

    update about

--- src/data/about.ts
+++ src/data/about.ts
@@ -10,20 +10,20 @@ export const about = {
         url: "https://github.com/Matheus1714.png",
         alt: "profile image"
     },
-    description: "I am a highly motivated individual with a Bachelor's degree in Aerospace Engineering with Honors from Instituto Tecnológico de Aeronáutica (ITA), currently pursuing a Master's degree in Computer Science at the same institute. My academic journey has equipped me with a strong foundation in aerospace engineering and a comprehensive understanding of computer science. My technical expertise extends to web development, where I focus on JavaScript (TypeScript, React, React Native, Node), Python (Flask, Django), and other frameworks such as Laravel and Flutter. During my professional experience as a Freelancer at Hastings Informática, I developed proficiency in Python, Selenium, Django, SQL Server Management Studio (SSMS), Docker, and Azure Git, enhancing my skills in web scraping and data extraction. As a Collaborating Researcher at Instituto Tecnológico de Aeronáutica, I contributed to user experience modeling using Laravel and PostgreSQL, showcasing my versatility in front-end development. Additionally, my role as a Developer at Quero Educação and ITA Júnior, among other positions, has provided me with extensive experience in application development, interface design, and training. With a solid background in aerospace engineering and a focus on computer science, I am well-equipped for opportunities in the IT market, particularly in web development, where I bring a wealth of practical skills and a commitment to delivering innovative solutions.",
+    description: "I am a highly motivated individual with a Bachelor's degree in Aerospace Engineering with Honors from Aeronautics Technological Institute (ITA), currently pursuing a Master's degree in Computer Science at the same institute. My academic journey has equipped me with a strong foundation in aerospace engineering and a comprehensive understanding of computer science. My technical expertise extends to web development, where I focus on JavaScript (TypeScript, React, React Native, Node), Python (Flask, Django), and other frameworks such as Laravel and Flutter. During my professional experience as a Freelancer at Hastings Informática, I developed proficiency in Python, Selenium, Django, SQL Server Management Studio (SSMS), Docker, and Azure Git, enhancing my skills in web scraping and data extraction. As a Collaborating Researcher at Aeronautics Technological Institute, I contributed to user experience modeling using Laravel and PostgreSQL, showcasing my versatility in front-end development. Additionally, my role as a Developer at Quero Educação and ITA Júnior, among other positions, has provided me with extensive experience in application development, interface design, and training. With a solid background in aerospace engineering and a focus on computer science, I am well-equipped for opportunities in the IT market, particularly in web development, where I bring a wealth of practical skills and a commitment to delivering innovative solutions.",
     experiences: [
         {
             startYear: 2018,
             endYear: 2023,
-            title: "Engenharia Aeroespacial",
-            content: "Instituto Tecnológico de Aeronáutica (ITA)",
+            title: "Aerospace Engineering",
+            content: "Aeronautics Technological Institute (ITA)",
             experienceType: EnumExperienceType.ACADEMIC,
         },
         {
             startYear: 2022,
             endYear: 2024,
-            title: "Mestrado em Ciência da Computação",
-            content: "Instituto Tecnológico de Aeronáutica (ITA)",
+            title: "Master in Computer Science",
+            content: "Aeronautics Technological Institute (ITA)",
             experienceType: EnumExperienceType.ACADEMIC,
         },
         // ----------------------------------------------------------
@@ -37,7 +37,7 @@ export const about = {
         {
             startYear: 2022,
             endYear: 2023,
-            title: "Instituto Tecnológico de Aeronáutica (Collaborating Researcher)",
+            title: "Aeronautics Technological Institute (Collaborating Researcher)",
             content: "I worked as an application developer, dedicating myself to modeling user experience. I developed Front-end skills using Laravel, and used the PostgreSQL database. Additionally, I took responsibility for developing user access models, as well as user experience.",
             experienceType: EnumExperienceType.PROFESSIONAL,
         },
