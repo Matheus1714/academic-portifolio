45f37ff4edc287c92d04c0f6c1e5a69bb1ba46e1
matheus1714 <matheussilvamartins1714@gmail.com>
2024-02-24 11:37:17 -0300

    config: storybook

---
+++ src/components/Logo/index.stories.tsx
@@ -0,0 +1,9 @@
+import { Logo } from './index'
+import { Meta, StoryObj } from '@storybook/react'
+
+export default {
+    title: 'Components/Logo',
+    component: Logo,
+} as Meta
+
+export const Default: StoryObj = {}
