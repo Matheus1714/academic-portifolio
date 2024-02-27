e12b0f5b207b730d0ee20f1f715913d11c40556e
matheus1714 <matheussilvamartins1714@gmail.com>
2024-02-26 22:38:00 -0300

    fix: error container class

--- src/components/Logo/styled.ts
+++ src/components/Logo/styled.ts
@@ -12,17 +12,3 @@ export const Container = styled.div`
         font-size: 0.875rem;
     }
 `;
-import styled from "styled-components";
-
-export const Container = styled.div`
-    h1{
-        color: ${(props) => props.theme.blue};
-        font-weight: 800;
-        font-size: 1.25rem;
-    }
-    p{
-        color: ${(props) => props.theme.white};
-        font-weight: 800;
-        font-size: 0.875rem;
-    }
-`
