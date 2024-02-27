ca0749f1bc157713fd9d1d7921d219f898e157a5
matheus1714 <matheussilvamartins1714@gmail.com>
2024-02-27 02:20:27 -0300

    feat: add configuration HOME

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,18 @@
 export const SITE = {
-    title: "MOTA, M. S. M",
-    subtitle: "Academic Portal"
-}
+  title: 'MOTA, M. S. M',
+  subtitle: 'Academic Portal',
+  author: {
+    fullName: 'Matheus Silva Martins Mota',
+    firstName: 'Matheus',
+    lastName: 'Mota',
+  },
+};
+
+export const HOME = {
+  firstText: SITE.author.firstName,
+  secondText: SITE.author.lastName,
+  phrase: {
+    text: '“Constant improvement is better than delayed perfection”',
+    author: 'Mark Tawain',
+  },
+};
